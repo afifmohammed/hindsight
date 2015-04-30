037ae3bc79e9aef9918c8de66f97facabee0ee1f
Afif Mohammed <afifmohammed@gmail.com>
2015-04-30 12:34:11 +1000

    Saga No DTC fix
    Allowing Sagas to work when they are retried owing to a failure with an
    external resource that is not covered in a distributed transaction. This
    approach relies on the saga handler being idempotent.

--- MediatR.Sagas/SagaNotificationHandler.cs
+++ MediatR.Sagas/SagaNotificationHandler.cs
@@ -36,6 +36,7 @@ namespace MediatR.Sagas
 
             if (this.innerHandler.Saga.MarkedAsComplete)
             {
+                this.innerHandler.Handle(notification);
                 Publish(new SagaOver<TNotification> { Content = notification });
                 return;
             }
